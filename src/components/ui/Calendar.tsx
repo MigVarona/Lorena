b8d6634939fb4c8ca84dd744a923a190849a0213
MigVarona <migvaronag@gmail.com>
2025-01-11 17:52:00 +0100

    pr

--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { DayPicker } from "react-day-picker";
+import { es } from "react-day-picker/locale";
+
 import "react-day-picker/dist/style.css";
 
 interface CalendarProps {
@@ -8,29 +10,24 @@ interface CalendarProps {
   className?: string;
 }
 
-export const Calendar: React.FC<CalendarProps> = ({
-  selected,
-  onSelect,
-  className = "",
-}) => {
+export const Calendar: React.FC<CalendarProps> = ({ selected, onSelect }) => {
   const isDisabled = (date: Date) => {
     const day = date.getDay();
-    return day === 6 || day === 0; 
+    return day === 1 || day === 0;
   };
   return (
     <div>
       <DayPicker
+        locale={es}
         mode="single"
         selected={selected}
         onSelect={(date) => {
-          if (!date || isDisabled(date)) return; 
+          if (!date || isDisabled(date)) return;
           onSelect(date);
         }}
         modifiers={{
           disabled: isDisabled, // Aquí aplicamos la lógica para días deshabilitados
         }}
-       
-        
       />
     </div>
   );
